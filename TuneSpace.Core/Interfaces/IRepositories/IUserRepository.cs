1a3f1746d730402cd416783e5f539da3f61b7e67
Shredrox <93082689+Shredrox@users.noreply.github.com>
2025-03-25 22:45:57 +0200

    update IUserRepository

--- TuneSpace.Core/Interfaces/IRepositories/IUserRepository.cs
+++ TuneSpace.Core/Interfaces/IRepositories/IUserRepository.cs
@@ -7,6 +7,7 @@ public interface IUserRepository
     Task<User?> GetUserById(string id);
     Task<User?> GetUserByEmail(string email);
     Task<User?> GetUserByName(string name);
+    Task<User?> GetUserByRefreshToken(string refreshToken);
     Task<List<string>> SearchByName(string name);
     Task InsertUser(User user, string password);
     Task UpdateUser(User user);
