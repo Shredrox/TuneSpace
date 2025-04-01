72f5686a47472731f0701b9b1f9d32a07ab56c0c
Shredrox <93082689+Shredrox@users.noreply.github.com>
2025-04-01 21:42:55 +0300

    expand BandRepository

--- TuneSpace.Core/Interfaces/IRepositories/IBandRepository.cs
+++ TuneSpace.Core/Interfaces/IRepositories/IBandRepository.cs
@@ -5,6 +5,10 @@ namespace TuneSpace.Core.Interfaces.IRepositories;
 public interface IBandRepository
 {
     Task InsertBand(Band band);
+    Task<List<Band>> GetAllBands();
+    Task<List<Band>> GetBandsByGenre(string genre);
+    Task<List<Band>> GetBandsByLocation(string location);
+    Task<List<Band>> GetBandsByGenreAndLocation(string genre, string location);
     Task<Band?> GetBandById(Guid id);
     Task<Band?> GetBandByName(string name);
     Task<Band?> GetBandByUserId(string id);
