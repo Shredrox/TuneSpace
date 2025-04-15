7ebfa4abde10e6b6026abdd7341f39a2a70abb58
Shredrox <93082689+Shredrox@users.noreply.github.com>
2025-04-15 20:12:39 +0300

    update repositories

--- TuneSpace.Infrastructure/Repositories/UserRepository.cs
+++ TuneSpace.Infrastructure/Repositories/UserRepository.cs
@@ -6,45 +6,50 @@ using TuneSpace.Infrastructure.Data;
 
 namespace TuneSpace.Infrastructure.Repositories;
 
-internal class UserRepository(UserManager<User> userManager, TuneSpaceDbContext context) : IUserRepository
+internal class UserRepository(
+    UserManager<User> userManager,
+    TuneSpaceDbContext context) : IUserRepository
 {
+    private readonly UserManager<User> _userManager = userManager;
+    private readonly TuneSpaceDbContext _context = context;
+
     async Task<User?> IUserRepository.GetUserById(string id)
     {
-        return await userManager.FindByIdAsync(id);
+        return await _userManager.FindByIdAsync(id);
     }
 
     async Task<User?> IUserRepository.GetUserByEmail(string email)
     {
-        return await userManager.FindByEmailAsync(email);
+        return await _userManager.FindByEmailAsync(email);
     }
 
     async Task<User?> IUserRepository.GetUserByName(string name)
     {
-        return await userManager.FindByNameAsync(name);
+        return await _userManager.FindByNameAsync(name);
     }
 
     async Task<User?> IUserRepository.GetUserByRefreshToken(string refreshToken)
     {
-        return await context.Users
+        return await _context.Users
             .FirstOrDefaultAsync(u => u.RefreshToken == refreshToken && u.RefreshTokenValidity > DateTime.Now.ToUniversalTime());
     }
 
     async Task<List<string>> IUserRepository.SearchByName(string name)
     {
-        return await userManager.Users
-            .Where(u => u.UserName.StartsWith(name))
-            .Select(u => u.UserName)
+        return await _userManager.Users
+            .Where(u => u.UserName != null && u.UserName.StartsWith(name))
+            .Select(u => u.UserName!)
             .Take(5)
             .ToListAsync();
     }
 
     async Task IUserRepository.InsertUser(User user, string password)
     {
-        await userManager.CreateAsync(user, password);
+        await _userManager.CreateAsync(user, password);
     }
 
     async Task IUserRepository.UpdateUser(User user)
     {
-        await userManager.UpdateAsync(user);
+        await _userManager.UpdateAsync(user);
     }
 }
