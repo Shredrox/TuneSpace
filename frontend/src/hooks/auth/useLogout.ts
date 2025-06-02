ff2def9d744415a1654eca91fd4c3ed3e5fe03c6
Shredrox <93082689+Shredrox@users.noreply.github.com>
2025-06-02 19:11:07 +0300

    add cache clear on logout

--- frontend/src/hooks/auth/useLogout.ts
+++ frontend/src/hooks/auth/useLogout.ts
@@ -1,14 +1,16 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { logout } from "@/services/auth-service";
 import useAuth from "./useAuth";
 
 const useLogout = () => {
   const { clearAuth } = useAuth();
+  const queryClient = useQueryClient();
 
   const handleLogout = useMutation({
     mutationFn: () => logout(),
     onSuccess: () => {
       clearAuth();
+      queryClient.clear();
     },
   });
 
