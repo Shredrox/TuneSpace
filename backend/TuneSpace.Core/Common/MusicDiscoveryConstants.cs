4f5144bbd64dc0864d89f48b3d1985a06c29a4f9
Shredrox <93082689+Shredrox@users.noreply.github.com>
2025-06-21 13:22:18 +0300

    update MusicDiscoveryConstants

--- backend/TuneSpace.Core/Common/MusicDiscoveryConstants.cs
+++ backend/TuneSpace.Core/Common/MusicDiscoveryConstants.cs
@@ -5,16 +5,17 @@ namespace TuneSpace.Core.Common;
 /// </summary>
 public static class MusicDiscoveryConstants
 {
-    public const int MaxRecommendations = 20;
+    public const int MaxRecommendations = 30;
     public const double RegisteredBandBonus = 0.5;
     public const double DiversityFactor = 0.15;
     public const double ExplorationFactor = 0.1;
     public const double UndergroundBandBonus = 0.25;
     public const double NewRegistrationBonus = 0.2;
     public const double MinRegisteredBandPercentage = 0.35;
-    public const int RecommendationCooldownDays = 14;
+    public const int RecommendationCooldownDays = 7;
     public const int MaxPopularityForUnderground = 30;
-    public const int UndergroundArtistsToFetch = 15;
+    public const int UndergroundArtistsToFetch = 25;
+    public const int UndergroundListenersThreshold = 10000;
 
     public const double GenreMatchScore = 0.3;
     public const double LocationMatchBonus = 0.3;
